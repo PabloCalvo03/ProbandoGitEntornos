0d3ae9f276a0665ebd18ca8a5e7893e0226b838a
Pablo Calvo <alum.pcalvoj.2022@iesalixar.org>
2023-02-17 16:43:27 +0100

    Mi segundo commit.

--- ProbandoGit/src/Principal.java
+++ ProbandoGit/src/Principal.java
@@ -3,7 +3,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		System.out.println("Hola mundo!");
-		System.out.println("Adios mundo!");
+		System.out.println("Adioos mundo!");
 	}
 
 }
